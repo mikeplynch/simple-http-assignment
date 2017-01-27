c525cb75b8e316f03cb04be241af53111162ec78
Michael Lynch <mpl2999@rit.edu>
2017-01-27 15:55:13 -0500

    stupid syntax

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ const onRequest = (request, response) => {
 
 http.createServer(onRequest).listen(port);
 
-console.log('Listening on 127.0.0.1: port');
+console.log(`Listening on 127.0.0.1: ${port}`);
 
